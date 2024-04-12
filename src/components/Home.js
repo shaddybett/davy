3247a4c1680aacd8f49aac19d1ef68329a1011f5
Shaddy <shadybett540@gmail.com>
2024-04-12 18:33:59 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Button } from "flowbite-react";
+import logo from "../assets/images/postgresql.svg";
+import fuel from "../assets/images/pxfuel (3).jpg";
 import "./home.css";
 
 function Home() {
   return (
-    <div className="home">
-      <h1 class="text-3xl font-bold underline ml-40">Hello world!</h1>
-      <Button>Click</Button>
+    <div className="body">
+      <div className="flex justify-center items-center flex-col">
+       
+        <img src={logo} alt="logo" className="w-10 mb-2" />
+      
+        <img src={fuel} alt="pxl" className="w-300 h-80" />
+        
+      </div>
+      <div className=" max-w-50% flex justify-center items-center flex-col">
+        <p>
+          heloo This is a cool web site to address all issues <br/> (including
+          breaking changes), run: hhjkdujdsnjjuuh
+        </p>
+      </div>
     </div>
   );
 }
