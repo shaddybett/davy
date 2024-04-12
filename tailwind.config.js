635700665bbd2f0f5223829c439d6e520274f130
Shaddy <shadybett540@gmail.com>
2024-04-12 15:15:10 +0300

    made home.js file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",flowbite.content()],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite.plugin(),],
+}
+
